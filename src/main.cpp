31a1135b8334f7c38355a2fd11b4b1e5ac11a011
Matteo Carnelos <matteo.carnelos@studenti.unipd.it>
2021-08-03 09:52:56 +0200

    First implementation done

--- src/main.cpp
+++ src/main.cpp
@@ -1,249 +1,98 @@
 #include <Arduino.h>
-
-// esp32 sdk imports
-#include "esp_heap_caps.h"
-#include "esp_log.h"
-
-// epd
+#include <ArduinoJson.h>
 #include "epd_driver.h"
 #include "epd_highlevel.h"
-
-// battery
-#include <driver/adc.h>
-#include "esp_adc_cal.h"
-
-// deepsleep
-#include "esp_sleep.h"
-
-// font
-#include "Firasans.h"
-#include "MonospaceMedium32.h"
-#include "DSEG14_Classic_Regular32.h"
-#include "DSEG14_Classic_Regular64.h"
-#include "DSEG14_Classic_Regular128.h"
-
-// sailtrack
+#include "DSEG14_Classic_Regular100.h"
 #include "SailtrackModule.h"
 
-#define BATT_PIN 36
+#define BATTERY_ADC_PIN 36
+#define BATTERY_ADC_MULTIPLIER 1.7
+
+#define MMS_TO_KNOTS_MULTIPLIER 0.00194384
 
 #define WAVEFORM EPD_BUILTIN_WAVEFORM
 
-SailtrackModule monitorModule;
+struct DisplaySlot {
+    char topicName[20];
+    char metricName[10];
+    int cursor_x;
+    int cursor_y;
+    EpdRect area;
+    double multiplier;
+} displaySlots[] = {
+    {"sensor/gps0", "speed", 500, 227, {713, 0, 227, 540}, MMS_TO_KNOTS_MULTIPLIER}
+};
 
-/**
- * Upper most button on side of device. Used to setup as wakeup source to start from deepsleep.
- * Pinout here https://ae01.alicdn.com/kf/H133488d889bd4dd4942fbc1415e0deb1j.jpg
- */
-gpio_num_t FIRST_BTN_PIN = GPIO_NUM_39;
-
+EpdFontProperties font_props = epd_font_properties_default();
+EpdRotation orientation = EPD_ROT_PORTRAIT;
 EpdiyHighlevelState hl;
-// ambient temperature around device
 int temperature = 20;
 uint8_t *fb;
-enum EpdDrawError err;
+unsigned long displayReady = millis();
+unsigned long displayClear = 200;
 
-// CHOOSE HERE YOU IF YOU WANT PORTRAIT OR LANDSCAPE
-// both orientations possible
-EpdRotation orientation = EPD_ROT_PORTRAIT;
-// EpdRotation orientation = EPD_ROT_LANDSCAPE;
-
-int vref = 1100;
-
-/**
- * RTC Memory var to get number of wakeups via wakeup source button
- * For demo purposes of rtc data attr
-**/
-RTC_DATA_ATTR int pressed_wakeup_btn_index;
-
-/**
- * That's maximum 30 seconds of runtime in microseconds
- */
-int64_t maxTimeRunning = 30000000;
-
-double_t get_battery_percentage()
-{
-    // When reading the battery voltage, POWER_EN must be turned on
-    epd_poweron();
-    delay(50);
-
-    Serial.println(epd_ambient_temperature());
-
-    uint16_t v = analogRead(BATT_PIN);
-    Serial.print("Battery analogRead value is");
-    Serial.println(v);
-    double_t battery_voltage = ((double_t)v / 4095.0) * 2.0 * 3.3 * (vref / 1000.0);
-
-    // Better formula needed I suppose
-    // experimental super simple percent estimate no lookup anything just divide by 100
-    double_t percent_experiment = ((battery_voltage - 3.7) / 0.5) * 100;
-
-    // cap out battery at 100%
-    // on charging it spikes higher
-    if (percent_experiment > 100)
-    {
-        percent_experiment = 100;
+void updateSlot(DisplaySlot slot, int value) {
+    if (millis() < displayReady) return;
+    if (!displayClear--) {
+        epd_clear();
+        displayClear = 200;
     }
-
-    String voltage = "Battery Voltage :" + String(battery_voltage) + "V which is around " + String(percent_experiment) + "%";
-    Serial.println(voltage);
-
-    epd_poweroff();
-    delay(50);
-
-    return percent_experiment;
-}
-
-void display_center_message(const char *text)
-{
-    // first set full screen to white
+    char digits[3];
+    int cursor_x = slot.cursor_x;
+    int cursor_y = slot.cursor_y;
+    sprintf(digits, "%d", value);
     epd_hl_set_all_white(&hl);
+    epd_write_string(&DSEG14_Classic_Regular100, digits, &cursor_x, &cursor_y, fb, &font_props);
+    epd_hl_update_screen(&hl, MODE_GL16, temperature);
+    displayReady = millis() + 500;
+}
 
-    int cursor_x = EPD_WIDTH / 2;
-    int cursor_y = EPD_HEIGHT / 2;
-    if (orientation == EPD_ROT_PORTRAIT)
-    {
-        // height and width switched here because portrait mode
-        cursor_x = EPD_HEIGHT / 2;
-        cursor_y = EPD_WIDTH / 2;
+class ModuleCallbacks: public SailtrackModuleCallbacks {
+	void onWifiConnectionBegin() {
+		// TODO: Notify user
+	}
+	
+	void onWifiConnectionResult(wl_status_t status) {
+		// TODO: Notify user
+	}
+
+	DynamicJsonDocument getStatus() {
+		DynamicJsonDocument payload(300);
+		JsonObject battery = payload.createNestedObject("battery");
+		JsonObject cpu = payload.createNestedObject("cpu");
+		battery["voltage"] = analogRead(BATTERY_ADC_PIN) * BATTERY_ADC_MULTIPLIER / 1000;
+		cpu["temperature"] = temperatureRead();
+		return payload;
+	}
+
+    void onMqttMessage(const char * topic, const char * message) {
+        DynamicJsonDocument payload(500);
+        deserializeJson(payload, message);
+        for (auto slot : displaySlots) {
+            if (!strcmp(topic, slot.topicName)) {
+                int value = round(payload[slot.metricName].as<int>() * slot.multiplier);
+                updateSlot(slot, value);
+            }
+        }
     }
-    EpdFontProperties font_props = epd_font_properties_default();
-    font_props.flags = EPD_DRAW_ALIGN_CENTER;
-    epd_write_string(&DSEG14_Classic_Regular64, text, &cursor_x, &cursor_y, fb, &font_props);
+};
 
-    epd_poweron();
-    err = epd_hl_update_screen(&hl, MODE_GC16, temperature);
-    delay(500);
-    epd_poweroff();
-    delay(1000);
-}
-
-void display_full_screen_left_aligned_text(const char *text)
-{
-    EpdFontProperties font_props = epd_font_properties_default();
-    font_props.flags = EPD_DRAW_ALIGN_CENTER;
-
-    // first set full screen to white
-    epd_hl_set_all_white(&hl);
-
-    /************* Display the text itself ******************/
-    // hardcoded to start at upper left corner
-    // with bit of padding
-    int cursor_x = EPD_HEIGHT/2;
-    int cursor_y = 200;
-    epd_write_string(&DSEG14_Classic_Regular64, text, &cursor_x, &cursor_y, fb, &font_props);
-    /********************************************************/
-
-    /************ Battery percentage display ****************/
-    // height and width switched here because portrait mode
-    int battery_cursor_x = EPD_WIDTH - 30;
-    int battery_cursor_y = EPD_HEIGHT - 10;
-    if (orientation == EPD_ROT_PORTRAIT)
-    {
-        // switched x and y constants in portrait mode
-        battery_cursor_x = EPD_HEIGHT - 10;
-        battery_cursor_y = EPD_WIDTH - 30;
-    }
-    EpdFontProperties battery_font_props = epd_font_properties_default();
-    battery_font_props.flags = EPD_DRAW_ALIGN_RIGHT;
-    String battery_text = String(get_battery_percentage());
-    battery_text.concat("% Battery");
-    epd_write_string(&FiraSans_12, battery_text.c_str(), &battery_cursor_x, &battery_cursor_y, fb, &battery_font_props);
-    /********************************************************/
-
-    epd_poweron();
-    err = epd_hl_update_screen(&hl, MODE_GC16, temperature);
-    delay(500);
-    epd_poweroff();
-    delay(1000);
-}
-
-/**
- * Powers off everything into deepsleep so device and display.
- */
-void start_deep_sleep_with_wakeup_sources()
-{
-    epd_poweroff();
-    delay(400);
-    esp_sleep_enable_ext0_wakeup(FIRST_BTN_PIN, 0);
-
-    Serial.println("Sending device to deepsleep");
-    esp_deep_sleep_start();
-}
-
-/**
- * Correct the ADC reference voltage. Was in example of lilygo epd47 repository to calc battery percentage
-*/
-void correct_adc_reference()
-{
-    esp_adc_cal_characteristics_t adc_chars;
-    esp_adc_cal_value_t val_type = esp_adc_cal_characterize(ADC_UNIT_1, ADC_ATTEN_DB_11, ADC_WIDTH_BIT_12, 1100, &adc_chars);
-    if (val_type == ESP_ADC_CAL_VAL_EFUSE_VREF)
-    {
-        Serial.printf("eFuse Vref:%u mV", adc_chars.vref);
-        vref = adc_chars.vref;
-    }
-}
-
-void setup()
-{
-    Serial.begin(115200);
-
-    correct_adc_reference();
-
-    monitorModule.init("sailtrack-monitor");
-
-    // First setup epd to use later
+void beginEPD() {
+    font_props.flags = EPD_DRAW_ALIGN_RIGHT;
     epd_init(EPD_OPTIONS_DEFAULT);
     hl = epd_hl_init(WAVEFORM);
     epd_set_rotation(orientation);
     fb = epd_hl_get_framebuffer(&hl);
     epd_clear();
-
-    int cursor_x = EPD_HEIGHT/2;
-    int cursor_y = DSEG14_Classic_Regular128.ascender + 5;
-
-    EpdRect rect = {.x = DSEG14_Classic_Regular128.ascender + 10, .y = 0, .width = 540, .height = DSEG14_Classic_Regular128.ascender + 15};
-    uint8_t color = 0;
-    epd_draw_rect(rect, color,fb);
-
-    EpdFontProperties font_props = epd_font_properties_default();
-    font_props.flags = EPD_DRAW_ALIGN_CENTER;
-
-    const char *text = "023"; 
-    
-    epd_write_string(&DSEG14_Classic_Regular64, text, &cursor_x, &cursor_y, fb, &font_props);
-
     epd_poweron();
-    err = epd_hl_update_screen(&hl, MODE_GC16, temperature);
-    delay(500);
-    epd_poweroff();
 }
 
-void loop()
-{
-    /* 
-    * Shutdown device after 30s always to conserve battery.
-    * Basically almost no battery usage then until next wakeup.
-    */
-
-    // Serial.printf("T0: %d\t", touchRead(T0)); // get value of Touch 0 pin = GPIO 4
-    // Serial.printf("T1: %d\t", touchRead(T1));
-    // Serial.printf("T2: %d\t", touchRead(T2));
-    // Serial.printf("T3: %d\t", touchRead(T3));
-    // Serial.printf("T4: %d\t", touchRead(T4));
-    // Serial.printf("T5: %d\t", touchRead(T5));
-    // Serial.printf("T6: %d\t", touchRead(T6));
-    // Serial.printf("T7: %d\t", touchRead(T7));
-    // Serial.printf("T8: %d\t", touchRead(T8));
-    // Serial.printf("T9: %d\n", touchRead(T9));
-    // delay(100);
-
-    // if (esp_timer_get_time() > maxTimeRunning) {
-    // Serial.println("Max runtime of 30s reached. Forcing deepsleep now to save energy");
-    // display_center_message("Sleeping now.\nWake me up from deepsleep again\nwith the first button on my side");
-    // delay(1500);
-
-    // start_deep_sleep_with_wakeup_sources();
-    // }
+void setup() {
+    beginEPD();
+    STModule.begin("monitor", "sailtrack-monitor", IPAddress(192,168,42,103));
+    STModule.setCallbacks(new ModuleCallbacks());
+    for (auto slot : displaySlots)
+        STModule.subscribe(slot.topicName);
 }
+
+void loop() { }

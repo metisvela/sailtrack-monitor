215d8539229d38cf75e238372377c4412a49a701
Matteo Carnelos <matteo.carnelos@studenti.unipd.it>
2022-03-07 01:58:54 +0100

    Changes:
    Improve font
    Add sign for zero-centered angles
    Fix margins

--- src/main.cpp
+++ src/main.cpp
@@ -3,9 +3,10 @@
 #include <epd_driver.h>
 #include <epd_highlevel.h>
 #include "DSEG14Classic_Regular_100.h"
-#include "BebasNeue_Regular_40.h"
+#include "SignsPlus.h"
+#include "SignsMinus.h"
+#include "Roboto_Bold_40.h"
 #include "SailtrackLogo.h"
-#include "MetisLogo.h"
 #include <SailtrackModule.h>
 
 #define BATTERY_ADC_PIN 36
@@ -18,14 +19,14 @@
 #define MONITOR_UPDATE_RATE 2
 
 #define MONITOR_SLOT_0 { 470, 227 }
-#define MONITOR_SLOT_1 { 470, 463 }
-#define MONITOR_SLOT_2 { 470, 690 }
-#define MONITOR_SLOT_3 { 470, 917 }
+#define MONITOR_SLOT_1 { 470, 454 }
+#define MONITOR_SLOT_2 { 470, 681 }
+#define MONITOR_SLOT_3 { 470, 908 }
 
 #define WAVEFORM EPD_BUILTIN_WAVEFORM
 #define CLEAR_MONITOR_CYCLES_INTERVAL 600
 
-enum MetricType { SPEED, ANGLE };
+enum MetricType { SPEED, ANGLE, ANGLE_ZERO_CENTERED };
 
 struct MonitorSlot {
     int cursorX;
@@ -44,7 +45,7 @@ struct MonitorMetric {
     {0, "sensor/gps0", "speed", "SOG", MMS_TO_KNOTS_MULTIPLIER, SPEED, MONITOR_SLOT_0},
     {0, "sensor/gps0", "heading", "COG", UDEGREE_TO_DEGREE_MULTIPLIER, ANGLE, MONITOR_SLOT_1},
     {0, "sensor/imu0", "euler.x", "HDG", IDENTITY_MULTIPLIER, ANGLE, MONITOR_SLOT_2},
-    {0, "sensor/imu0", "euler.y", "RLL", IDENTITY_MULTIPLIER, ANGLE, MONITOR_SLOT_3}
+    {0, "sensor/imu0", "euler.y", "RLL", IDENTITY_MULTIPLIER, ANGLE_ZERO_CENTERED, MONITOR_SLOT_3}
 };
 
 SailtrackModule STM;
@@ -57,14 +58,6 @@ int updateCycles = 0;
 uint8_t *fb;
 
 class ModuleCallbacks: public SailtrackModuleCallbacks {
-	void onWifiConnectionBegin() {
-		// TODO: Notify user
-	}
-	
-	void onWifiConnectionResult(wl_status_t status) {
-		// TODO: Notify user
-	}
-
 	DynamicJsonDocument getStatus() {
 		DynamicJsonDocument payload(300);
 		JsonObject battery = payload.createNestedObject("battery");
@@ -103,8 +96,7 @@ void beginEPD() {
     epd_poweron();
     if (esp_sleep_get_wakeup_cause() == ESP_SLEEP_WAKEUP_UNDEFINED) {
         epd_clear();
-        epd_draw_rotated_image({130, 320, SailtrackLogo_width, SailtrackLogo_height}, SailtrackLogo_data, fb);
-        epd_draw_rotated_image({203, 880, MetisLogo_width, MetisLogo_height}, MetisLogo_data, fb);
+        epd_draw_rotated_image({130, 350, SailtrackLogo_width, SailtrackLogo_height}, SailtrackLogo_data, fb);
         epd_hl_update_screen(&hl, MODE_GL16, temperature);
     }
 }
@@ -119,18 +111,30 @@ void setup() {
 void loop() { 
     epd_hl_set_all_white(&hl);
     for (auto metric : monitorMetrics) {
-        char digits[5];
-        char displayName[7];
-        int cursorX = metric.slot.cursorX;
-        int cursorY = metric.slot.cursorY;
+        char digits[4];
+        char displayName[6];
+        int cursorX;
+        int cursorY;
+
+        if (metric.type == ANGLE_ZERO_CENTERED) {
+            cursorX = 15;
+            cursorY = metric.slot.cursorY - 153;
+            if (metric.value >= 0) epd_draw_rotated_image({cursorX, cursorY, SignsPlus_width, SignsPlus_height}, SignsPlus_data, fb);
+            else epd_draw_rotated_image({cursorX, cursorY, SignsMinus_width, SignsMinus_height}, SignsMinus_data, fb);
+            metric.value = abs(metric.value);
+        }
+
         sprintf(digits, metric.type == SPEED ? "%.1f" : "%.0f", metric.value);
         fontProps.flags = EPD_DRAW_ALIGN_RIGHT;
+        cursorX = metric.slot.cursorX;
+        cursorY = metric.slot.cursorY;
         epd_write_string(&DSEG14Classic_Regular_100, digits, &cursorX, &cursorY, fb, &fontProps);
+
+        sprintf(displayName, "%c\n%c\n%c", metric.displayName[0], metric.displayName[1], metric.displayName[2]);
         fontProps.flags = EPD_DRAW_ALIGN_CENTER;
         cursorX = metric.slot.cursorX + 33;
         cursorY = metric.slot.cursorY - 140;
-        sprintf(displayName, "%c\n%c\n%c", metric.displayName[0], metric.displayName[1], metric.displayName[2]);
-        epd_write_string(&BebasNeue_Regular_40, displayName, &cursorX, &cursorY, fb, &fontProps);
+        epd_write_string(&Roboto_Bold_40, displayName, &cursorX, &cursorY, fb, &fontProps);
     }
     if (!updateCycles) {
         epd_clear();
@@ -139,4 +143,4 @@ void loop() {
     epd_hl_update_screen(&hl, updateCycles ? MODE_GL16 : MODE_EPDIY_WHITE_TO_GL16, temperature);
     updateCycles = (updateCycles + 1) % CLEAR_MONITOR_CYCLES_INTERVAL;
     delay(1000 / MONITOR_UPDATE_RATE);
- }
+}
